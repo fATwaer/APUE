cd3a40d4389ef1de9644791ab0f61267718de560
cFlowers <823265940@qq.com>
2018-04-18 03:44:08 -0700

    two way to read buf, and get pid which is long int type

--- chapter1/1-3.c
+++ chapter1/1-3.c
@@ -1,5 +1,6 @@
 #include "apue.h"
 #include <dirent.h>
+
 int
 main(int argc, char** argv[])
 {
---
+++ chapter1/1-6.c
@@ -0,0 +1,8 @@
+#include "apue.h"
+
+int
+main(void)
+{
+	printf("process's id(type long int): %ld\n", (long)getpid());
+	exit(0);
+}
