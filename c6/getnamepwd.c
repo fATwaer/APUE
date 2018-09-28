231823ca3238fc50c3e3e6e16d60a59f0615e09b
fATwaer <823265940@qq.com>
2018-09-28 23:22:36 +0800

    passwd file read

---
+++ c6/getnamepwd.c
@@ -0,0 +1,24 @@
+#include <pwd.h>
+#include <stddef.h>
+#include <string.h>
+
+struct passwd *
+getpwnam(const char *name)
+{
+    struct passwd *ptr;
+
+    setpwent();
+    while ((ptr = getpwent()) != NULL)
+        if (strcmp(ptr->pw_name, name))
+            break;
+    endpwent();
+    return (ptr);
+}
+
+int
+main()
+{
+    struct passwd *p = getpwnam("moonlight");
+
+    printf("passwd: %s dir: %s\n", p->pw_passwd, p->pw_dir);
+}
