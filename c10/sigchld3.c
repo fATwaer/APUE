fc7373c377c9f6e5999801275eba28011728cd7b
fATwaer <823265940@qq.com>
2018-10-09 09:20:39 +0800

    sigchld

---
+++ c10/sigchld3.c
@@ -0,0 +1,31 @@
+#include "apue.h"
+#include <errno.h>
+#include <sys/wait.h>
+
+static void
+handler(int signo)
+{
+    printf("SIGCHILD received\n");
+    for (;;);
+}
+
+
+int
+main()
+{
+    pid_t pid;
+
+    if ((pid = fork()) < 0)
+        err_sys("fork error");
+    else if (pid == 0) {
+        sleep(2);
+        exit(0);
+    }
+
+    wait(NULL);
+    pause();
+    exit(0);
+}
+
+
+
